3071019ad7185d5c5a70c1d20916715aa4b4e7ce
RITISHM <ritishmahajan15f@gmail.com>
2026-08-07 10:41:21 +0530

    Implement fibonacci generator function in JavaScript

---
+++ week5/Q28.js
@@ -0,0 +1,19 @@
+function* fibonacci() {
+  let a = 0;
+  let b = 1;
+  while (true) {
+    yield a;
+    let temp = a + b;
+    a = b;
+    b = temp;
+  }
+}
+
+const fibGen = fibonacci();
+
+console.log(fibGen.next().value);
+console.log(fibGen.next().value);
+console.log(fibGen.next().value);
+console.log(fibGen.next().value);
+console.log(fibGen.next().value);
+console.log(fibGen.next().value);
